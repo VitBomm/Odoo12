dc8eecc3dc3eb68d2843794cc9e269619ddb4cef
OSX <nnphucthinh@gmail.com>
2019-07-11 08:57:33 +0700

    Finallllll

--- wizard/cost_wizard.py
+++ wizard/cost_wizard.py
@@ -4,11 +4,11 @@ class CostWizard(models.TransientModel):
     _name = "hhd.cost.recovery.wizard"
     _description = "Thêm Note"
 
-    cost_id = fields.Many2one('hhd.cost.recovery', string="Chi Phí", default=default_cost)
-    field_note = fields.Text()
-
     def default_cost(self):
         return self.env['hhd.cost.recovery'].browse(self._context.get('note_field'))
+
+    cost_id = fields.Many2one('hhd.cost.recovery', string="Chi Phí", default=default_cost)
+    field_note = fields.Text()
 
     @api.multi
     def change_field(self):
